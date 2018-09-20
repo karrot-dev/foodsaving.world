63785b480c66ad96abe0bc132455114a560eb8e5
djahnie <djahnie@disroot.org>
2018-09-20 18:09:00 +0200

    change deployment from ci to git_pull

---
+++ git_pull.php
@@ -0,0 +1,28 @@
+<?php
+echo "starting...\n";
+try
+{
+  $payload = json_decode($_REQUEST['payload']);
+}
+catch(Exception $e)
+{
+  exit(0);
+}
+
+//log the request
+file_put_contents('logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
+
+
+if ($payload->ref === 'refs/heads/master')
+{
+  echo "pulling from master...\n";
+  // path to your site deployment script
+  system('git pull', $retval);
+  echo "exit code ".$retval."\n";
+  system('composer install', $retval);
+  echo "exit code ".$retval."\n";
+  system('./bin/grav clear-cache', $retval);
+  echo "exit code ".$retval."\n";
+}
+
+echo "finished.\n";
